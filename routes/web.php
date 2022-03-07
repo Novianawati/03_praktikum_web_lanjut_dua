8e166852ba47ea8f71b195911ae74a1ab2acaa9f
Novianawati <novianawati92@gmailcom>
2022-03-08 05:11:37 +0700

    changes

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
+
+Route::get('/about-us', function () {
+    return view('about');
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
